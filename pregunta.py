359ecc2b9892aa1c5eed6208462ee52be3800fa2
jchicaf <jchicaf@unal.edu.co>
2023-10-13 22:14:34 -0500

    Solution LAB 5

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,30 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
 
     #
     # Inserte su código aquí
     #
 
+    #borrar todas las filas con espacios vacios
+    df.dropna(inplace=True)
+
+    #convertir todo a minisculas
+    df.loc[:, df.dtypes=='object']=df.loc[:, df.dtypes=='object'].apply(lambda row: row.str.lower())
+
+    #reemplazar
+    df=df.apply(lambda x: x.astype(str).str.replace("-"," ").str.replace("_"," ").str.replace("$","").str.replace(",",""))
+
+    #covertir a fecha
+    df.fecha_de_beneficio = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
+
+    #convertir float
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+
+    #borrar los duplicados
+    df.drop_duplicates(inplace=True)
+
     return df
+
+
